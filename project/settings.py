0ac85eaa7afeb03e6c9c58ba4d0f7d2fa41fbebc
Seth <mrsethgabriel@gmail.com>
2024-05-01 17:20:30 +0800

    Remove allowed host

--- project/settings.py
+++ project/settings.py
@@ -29,7 +29,7 @@ SECRET_KEY = config('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ['159.65.8.149']
+ALLOWED_HOSTS = []
 
 # Application definition
 
