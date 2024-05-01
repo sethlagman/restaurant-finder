a912eac9835e8f3ff9cc0620aa75a8a8b1d05c57
Seth <mrsethgabriel@gmail.com>
2024-05-01 23:22:51 +0800

    Modify allowed hosts

--- project/settings.py
+++ project/settings.py
@@ -27,7 +27,7 @@ SECRET_KEY = config('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ['*']
+ALLOWED_HOSTS = ['*', 'restaurant-finder-q6kfa.ondigitalocean.app/']
 
 # Application definition
 
