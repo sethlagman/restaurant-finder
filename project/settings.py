5327b0174a4b2f53be196b46de341ae7deee4c79
Seth <mrsethgabriel@gmail.com>
2024-05-01 17:05:44 +0800

    Changed allowed_hosts

--- project/settings.py
+++ project/settings.py
@@ -29,7 +29,7 @@ SECRET_KEY = config('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ['178.128.22.131']
+ALLOWED_HOSTS = ['159.65.8.149']
 
 # Application definition
 
